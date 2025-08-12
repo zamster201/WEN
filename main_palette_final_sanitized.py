17bc00444e3f648a82311a6299386f827f4bcc43
Tom Knopp <zamster201@gmail.com>
2025-08-11 22:56:59 -0400

    Initial cleanup and commit

--- main_palette_final_sanitized.py
+++
@@ -1,3 +0,0 @@
-
-# This is a placeholder stub. In actual execution, this would contain the corrected main_palette_full_fixed.py content
-# with the line removed: icon_path = self.theme_mgr.get_icon_path(...) in __init__.
